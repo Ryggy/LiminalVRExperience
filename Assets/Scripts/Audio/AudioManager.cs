f60e604c40980a9f33c33f33c99dde4b0ddeb399
DaLucanator <lucgrfths@gmail.com>
2025-04-17 15:14:13 +1000

    started audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] bool playerIsInteracting;
+    [SerializeField] AudioSource audioInteract;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerIsInteracting && audioInteract.volume < 1)
+        {
+            audioInteract.volume += 0.01f;
+        }
+
+        else if (!playerIsInteracting && audioInteract.volume > 0)
+        {
+            audioInteract.volume -= 0.01f;
+        }
+    }
+
+    //call this when the player starts or stops interacting with the flowfield
+    public void PlayerInteract()
+    {
+        playerIsInteracting = !playerIsInteracting;
+    }
+
+
+}

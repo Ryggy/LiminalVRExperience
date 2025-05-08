30425ffc9e9cf1772e7ddad7529e1821ad165232
Ryan W <>
2025-05-08 19:03:22 +1000

    Added 3D flow field with .fga file support
    - Implemented methods for loading and sampling vector data from .fga files
    - Added particle emitter system to spawn and initialise particles in the field volume
    - Created particle controller that moves particles using sampled vector directions
    - Add optional Rigidbody controller for physics-based flow response
    - Include custom editor for selecting and loading .fga files from project
    - Add Gizmo visualisation for field vectors with multiple display modes

---
+++ Assets/Scripts/FlowField/3D/FlowFieldRigidBodyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Vector Field/Rigidbody Controller")]
+[ExecuteInEditMode]
+[RequireComponent(typeof(Rigidbody))]
+public class FlowFieldRigidBodyController : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float Tightness=1.0f;
+    [Range(0.05f, 10f)]
+    public float MinimalInfluence = 0.05f;
+    public float Multiplier = 1.0f;
+    public bool AffectedByAllFF = true;
+    public List<FlowFieldVolume> FFRestrictedList = new List<FlowFieldVolume>();
+
+    Rigidbody rb;
+    
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    
+    private float hlslSmoothstep(float min, float max, float value)
+    {
+        float t = Mathf.Clamp01((value - min) / (max - min));
+        return t * t * (3.0f - 2.0f * t);
+    }
+	
+    void Update ()
+    {
+        Vector3 force;
+
+        if (AffectedByAllFF)
+            force = FlowFieldVolume.GetCombinedVectors(transform.position)*Multiplier;
+        else
+            force = FlowFieldVolume.GetCombinedVectorsRestricted(transform.position, FFRestrictedList)*Multiplier;
+
+        float intensity = force.magnitude;
+        float blendIntensity = hlslSmoothstep(0.0f, MinimalInfluence+0.0001f, intensity);
+
+        float SQTightness = Tightness*Tightness;
+			
+        rb.velocity = Vector3.Lerp(rb.velocity, force, SQTightness*blendIntensity);
+        rb.AddForce(force*(1.0f - SQTightness)*blendIntensity, ForceMode.Force);
+    }
+}
